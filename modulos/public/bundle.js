4dee8ec0b62e31f22a37a746cdb15e375bca4cf0
SergioSanchezPerez19 <s.sanchezperezgarcia@gmail.com>
2024-04-19 12:45:25 +0200

    first commit

---
+++ modulos/public/bundle.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Mediante la palabra exports
+// export const name = 'Carlos';
+
+// export const obtenerPost = () => {
+//     return ['Post1'];
+// };
+
+//
+
+const name = 'Carlos';
+
+const obtenerPost = () => {
+    return ['Post1'];
+};
+
+console.log(name);
+
+console.log(obtenerPost());
